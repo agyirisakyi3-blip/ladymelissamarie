647cd444fe7b554cf1faa0f4ce01f5d81db8c6d1
agyiri sakyi <info@ezranetwork.com>
2026-05-02 14:35:22 +0000

    Add PWA support with manifest.json and service worker

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'lady-melissa-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/about.html',
+    '/teachings.html',
+    '/mission.html',
+    '/ventures.html',
+    '/credentials.html',
+    '/contact.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).then((networkResponse) => {
+                if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
+                    const responseToCache = networkResponse.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, responseToCache);
+                    });
+                }
+                return networkResponse;
+            });
+        }).catch(() => {
+            if (event.request.destination === 'document') {
+                return caches.match('/index.html');
+            }
+        })
+    );
+});
